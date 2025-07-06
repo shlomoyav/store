46e1339182731845d248826a98ba4a7fa662421c
shlomoyav <91040898+shlomoyav@users.noreply.github.com>
2025-07-06 21:59:40 +0300

    Add files via upload

---
+++ infrastucture/Store.Memory/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        //BookRepository repository;
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refacroring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
